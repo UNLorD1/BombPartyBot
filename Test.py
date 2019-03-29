65b297f13d15ef4fded11ca4ea8aec01490f43b5
UNLorD1 <alexhuynh3@gmail.com>
2019-03-28 21:58:21 -0700

    added git commands.html

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-#test
+'''
+Plan:
+- Make a discord bot
+- Make a test dictionary (5 words or so)
+- Make the timer 10 seconds
+- 2 or 3 lives
+
+
+- Extra: When all letters are used, get +1 life. 3 max.
+'''
