fa292b3a69ef7a2e5d750375f60aed5d84d655f3
UNLorD1 <alexhuynh3@gmail.com>
2019-03-28 22:07:34 -0700

    added git commands.html

--- Test.py
+++ Test.py
@@ -1,10 +1,25 @@
 '''
-Plan:
+---LOGISTICS---
 - Make a discord bot
-- Make a test dictionary (5 words or so)
-- Make the timer 10 seconds
-- 2 or 3 lives
+- Make a test dictionary (apple, please, lease, fetch, chap)
+- Make the timer 10 seconds (make timer visible)
+- 2 lives
 
+---LOGIC---
+- Generate and display a 2-3 letter string
+    - String must exist within a word in dictionary
+- Input = user input
+- 2 checks:
+    - Check if user input contains String
+    - If yes check if user input matches word in dictionary
+- If both checks = true, move to next person and reset timer.
+- If any checks = false, refuse word.
+- If timer = 0, remove life from player, move to next player. Repeat same string.
+    - If all players fail same string, reveal word with answer. Generate new string.
 
-- Extra: When all letters are used, get +1 life. 3 max.
+---EXTRA---
+- When all letters are used, get +1 life. 3 max.
+- Add settings page to edit:
+    - Words used (categories such as drug names)
+    - Timer duration
 '''
