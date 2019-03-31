2750f25b30d24552499792004e900f06b59b2b21
UNLorD1 <alexhuynh3@gmail.com>
2019-03-30 17:11:20 -0700

    Modified notdiscord.py

--- notdiscord.py
+++ notdiscord.py
@@ -1,21 +1,44 @@
-import discord
-from discord.ext import commands
+import discord  # importing in discord library
+from discord.ext import commands  # enables programming functionality
 
-TOKEN = NTU0MjI0NDU4ODc4MTU2ODEw.D39c0g.s32T5763G5h0YedwiPO_15J2i-k
-bot = commands.Bot(command_prefix='!')
+TOKEN = 'NTU0MjI0NDU4ODc4MTU2ODEw.D39c0g.s32T5763G5h0YedwiPO_15J2i-k'  # unique ID code for bot
+
+bot = commands.Bot(command_prefix='!')  # sets command prefix to !
 
 
-@bot.event
-async def on_ready():
+@bot.event  # discord controls when they're called.
+async def on_ready():  # when the bot is done collecting information from discord, servers, etc., do the following:
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
 
+@bot.event
+async def on_message(message):  # displays messages users send on discord
+    author = message.author  # captures user
+    content = message.content  # captures message
+    channel = message.channel  # captures channel
+    print('{}: {}: {}'.format(channel, author, content))  # prints author and message on console
+
+
+# @bot.event
+# async def on_message_delete(message):
+#     author = message.author  # captures user
+#     content = message.content  # captures message
+#     channel = message.channel  # captures channel
+#     await bot.send_message(channel, '{} tried to delete: {}'.format(author, content))
+
+
+# @bot.command()
+# async def space(ctx):
+#     msg = msg_fixer(ctx.message.content)
+#     await ctx.send(' '.join(msg))
+
+
 @bot.command()
-async def space(ctx):
-    msg = msg_fixer(ctx.message.content)
-    await ctx.send(' '.join(msg))
+async def ping():
+    await bot.say('Pong!')
+
 
 bot.run(TOKEN)
